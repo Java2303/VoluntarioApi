052539dc0011f518fe31877aa7bfe79f09432966
Java2303 <134702947+Java2303@users.noreply.github.com>
2025-06-21 13:47:03 -0400

    Update NotificacionEvento.cs

--- VolunteerApi/Models/NotificacionEvento.cs
+++ VolunteerApi/Models/NotificacionEvento.cs
@@ -1,6 +1,5 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using System;
 
 namespace VolunteerApi.Models
 {
@@ -10,16 +9,8 @@ namespace VolunteerApi.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        [BsonElement("mensaje")]
         public string Mensaje { get; set; }
 
-        [BsonElement("fecha")]
         public DateTime Fecha { get; set; }
-
-        [BsonElement("eventoId")]
-        public int EventoId { get; set; }
-
-        [BsonElement("leido")]
-        public bool Leido { get; set; } = false;
     }
 }
