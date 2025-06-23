b52da88905415424dd7070cbdcdeecf325efc01d
Java2303 <134702947+Java2303@users.noreply.github.com>
2025-06-23 19:54:07 -0400

    Create CursoDto.cs

---
+++ VolunteerApi/DTOs/CursoDto.cs
@@ -0,0 +1,12 @@
+namespace VolunteerApi.Dtos;
+
+public class CursoDto
+{
+    public int CursoId { get; set; }
+    public string Nombre { get; set; } = null!;
+    public DateOnly FechaInicio { get; set; }
+    public DateOnly FechaFin { get; set; }
+    public string? Categoria { get; set; }
+    public string? Dificultad { get; set; }
+    public string? ImagenUrl { get; set; }
+}
