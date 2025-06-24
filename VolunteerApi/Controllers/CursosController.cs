93a93d9074618f6efa4b5d649090f56adee45239
Java2303 <134702947+Java2303@users.noreply.github.com>
2025-06-23 20:00:34 -0400

    Update CursosController.cs

--- VolunteerApi/Controllers/CursosController.cs
+++ VolunteerApi/Controllers/CursosController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VolunteerApi.Models;
+using VolunteerApi.DTOs;
 
 namespace VolunteerApi.Controllers
 {
@@ -40,7 +41,7 @@ namespace VolunteerApi.Controllers
 
         // PUT: api/Cursos/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCurso(int id, CursoDto cursoDto)
+        public async Task<IActionResult> PutCurso(int id, CursoDTO cursoDto)
         {
             if (id != cursoDto.CursoId)
                 return BadRequest();
@@ -49,7 +50,7 @@ namespace VolunteerApi.Controllers
             if (curso == null)
                 return NotFound();
         
-            // Actualizar solo las propiedades permitidas
+            // Actualiza los campos
             curso.Nombre = cursoDto.Nombre;
             curso.FechaInicio = cursoDto.FechaInicio;
             curso.FechaFin = cursoDto.FechaFin;
@@ -57,11 +58,20 @@ namespace VolunteerApi.Controllers
             curso.Dificultad = cursoDto.Dificultad;
             curso.ImagenUrl = cursoDto.ImagenUrl;
         
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CursoExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
         
             return NoContent();
         }
-
 
         // POST: api/Cursos
         [HttpPost]
